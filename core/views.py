ca2da49fe365b30dd31dd2c70b86b054c5fb8e37
Sapargaliev Dias <dias_2003_2010@mail.ru>
2023-07-08 22:18:54 +0600

    added product display

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from item.models import Category, Product
+
 # Create your views here.
 
 def index(request):
-  return render(request, "core/index.html")
+  products = Product.objects.filter(is_sold=False)[0:6]
+  categories = Category.objects.all()
+
+  return render(request, "core/index.html", {
+    'categories': categories,
+    'products': products,
+  })
