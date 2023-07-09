f2857986457922770546125399fc04683eb3c16e
Sapargaliev Dias <dias_2003_2010@mail.ru>
2023-07-09 23:09:02 +0600

    added categories

--- aliexpress/urls.py
+++ aliexpress/urls.py
@@ -6,5 +6,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core', include('core.urls'))
+    path('', include('core.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
