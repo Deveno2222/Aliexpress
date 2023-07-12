47441a931d87adafae8801ad3b2bb6c875cba7ce
Sapargaliev Dias <dias_2003_2010@mail.ru>
2023-07-12 19:14:07 +0600

    modified core

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ app_name='core'
 urlpatterns = [
   path('', views.index, name='index'),
   path('signup/', views.signup, name='signup'),
+  path('logout/', views.logout_view, name='logout'),
   path('login/', auth_views.LoginView.as_view(template_name='core/login.html',authentication_form=LoginForm), name='login'),
 ]
