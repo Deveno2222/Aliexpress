746a6d76714b3c87dbf3226ed59c4b6826630e3d
Sapargaliev Dias <dias_2003_2010@mail.ru>
2023-07-12 17:46:58 +0600

    added path to dashboard

--- aliexpress/urls.py
+++ aliexpress/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('', include('core.urls'), name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
