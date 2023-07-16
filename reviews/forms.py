2e50ab88b391703a966944d7478fdc5cb5d342a9
Sapargaliev Dias <dias_2003_2010@mail.ru>
2023-07-16 21:22:36 +0600

    updated reviews forms

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,7 +2,15 @@ from django import forms
 
 from .models import Review
 
+INPUT_CLASSES = 'w-100 py-1 px-2 rounded border'
+
 class ReviewForm(forms.ModelForm):
   class Meta:
     model = Review
     fields = ('content',)
+
+    widgets = {
+      'content': forms.Textarea(attrs={
+        'class': INPUT_CLASSES,
+      }),
+    }
