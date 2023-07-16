cf151a78bccc99b85b3971a01dcf7c4e035e17ca
Sapargaliev Dias <dias_2003_2010@mail.ru>
2023-07-16 21:23:35 +0600

    fixed views.py

--- item/views.py
+++ item/views.py
@@ -2,17 +2,33 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from .forms import NewProductForm, EditProductForm
+from reviews.forms import ReviewForm
 
 # Create your views here.
 def detail(request, pk):
   product = get_object_or_404(Product, pk=pk)
   related_products = Product.objects.filter(category=product.category, is_sold=False).exclude(pk=pk)[0:3]
 
+  
+  if request.method == 'POST' and request.user.is_authenticated:
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+      review = form.save(commit=False)
+      review.user = request.user
+      review.product = product
+      review.save()
+      return redirect('item:detail', pk=pk)
+  else:
+    form = ReviewForm()
+
+
 
   return render(request, 'item/detail.html', {
     'product': product,
     'related_products': related_products,
+    'form': form,
   })
+
 
 @login_required
 def create(request):
